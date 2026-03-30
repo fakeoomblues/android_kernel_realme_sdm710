be8b14ebcd3e3ff2cbf724648d97147851fbe1b1
Aziz Nugroho <96572870+EZPZFORME@users.noreply.github.com>
2026-03-30 12:16:27 +0000

    mm/vmscan: Bump swappiness

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -163,7 +163,7 @@ unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+int vm_swappiness = 80;
 
 #ifdef CONFIG_OPLUS_MM_HACKS
 /*
