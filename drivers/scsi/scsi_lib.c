0b0254ea0b01c936e80b7f4c3bcbbc8927987e34
fakeoomblues <53104706+fakeoomblues@users.noreply.github.com>
2026-04-03 14:36:22 +0000

    scsi: core: Do not retry I/Os during depopulation
    Fail I/Os instead of retry to prevent user space processes from being
    blocked on the I/O completion for several minutes.
    
    Retrying I/Os during "depopulation in progress" or "depopulation restore in
    progress" results in a continuous retry loop until the depopulation
    completes or until the I/O retry loop is aborted due to a timeout by the
    scsi_cmd_runtime_exceeced().
    
    Depopulation is slow and can take 24+ hours to complete on 20+ TB HDDs.
    Most I/Os in the depopulation retry loop end up taking several minutes
    before returning the failure to user space.

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -920,9 +920,13 @@ void scsi_io_completion(struct scsi_cmnd *cmd, unsigned int good_bytes)
 				case 0x08: /* Long write in progress */
 				case 0x09: /* self test in progress */
 				case 0x11: /* notify (enable spinup) required */
-				case 0x14: /* space allocation in progress */
 					action = ACTION_DELAYED_RETRY;
 					break;
+				/*
+				 * Depopulation might take many hours,
+				 * thus it is not worthwhile to retry.
+				 */
+				case 0x24: /* depopulation in progress */
 				default:
 					action = ACTION_FAIL;
 					break;
